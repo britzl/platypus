f7b3cdfc60cff2ce8df733bcc2434a6b039e4ac6
Jean-Sébastien L <jeangit@users.noreply.github.com>
2021-10-06 23:22:46 +0200

    Jseb (#24)
    * menu for sample selection
    
    * player is a ghost, in variable raycast collisions demo
    
    * working with platform collisions (thank you JustAPotota)
    
    * player moving and crouching
    
    * small simplification in RAYS definition and looping
    
    * added a function « define_collisions » to platypus + sample
    
    * corrections asked on https://github.com/britzl/platypus/pull/24
    
    * added doc for define_collisions function
    
    * fix slopes GO speed (the steepier was the slope, the faster was the GO)
    
    * fix for requested changes on https://github.com/britzl/platypus/pull/24
    
    * fix the scale problem with the gui and the ugly fonts
    
    * Adjusted a few bits and pieces
    
    Co-authored-by: Björn Ritzl <bjorn.ritzl@gmail.com>

--- platypus/platypus.lua
+++ platypus/platypus.lua
@@ -130,10 +130,7 @@ function M.create(config)
 	-- movement based on user input
 	local movement = vmath.vector3()
 
-	local BOUNDS_BOTTOM = vmath.vector3(0, -platypus.collisions.bottom, 0)
-	local BOUNDS_TOP = vmath.vector3(0, platypus.collisions.top, 0)
-	local BOUNDS_LEFT = vmath.vector3(-platypus.collisions.left, 0, 0)
-	local BOUNDS_RIGHT = vmath.vector3(platypus.collisions.right, 0, 0)
+	local BOUNDS_BOTTOM, BOUNDS_TOP, BOUNDS_LEFT, BOUNDS_RIGHT = 0,0,0,0
 
 	local RAY_CAST_LEFT_ID = 1
 	local RAY_CAST_RIGHT_ID = 2
@@ -143,29 +140,36 @@ function M.create(config)
 	local RAY_CAST_DOWN_ID = 6
 	local RAY_CAST_DOWN_LEFT_ID = 7
 	local RAY_CAST_DOWN_RIGHT_ID = 8
+ 
+	local RAYS = {}
 
-	local RAY_CAST_LEFT = BOUNDS_LEFT + vmath.vector3(-1, 0, 0)
-	local RAY_CAST_RIGHT = BOUNDS_RIGHT + vmath.vector3(1, 0, 0)
-	local RAY_CAST_DOWN = BOUNDS_BOTTOM + vmath.vector3(0, -1, 0)
-	local RAY_CAST_UP = BOUNDS_TOP + vmath.vector3(0, 1, 0)
-	local RAY_CAST_DOWN_LEFT = RAY_CAST_LEFT + RAY_CAST_DOWN
-	local RAY_CAST_DOWN_RIGHT = RAY_CAST_RIGHT + RAY_CAST_DOWN
-	local RAY_CAST_UP_LEFT = RAY_CAST_UP + RAY_CAST_LEFT
-	local RAY_CAST_UP_RIGHT = RAY_CAST_UP + RAY_CAST_RIGHT
+	-- Change the rays and bounds for collision detection
+	-- @param collisions table with top,left,right,bottom keys
+	function platypus.set_collisions(collisions)
+		BOUNDS_BOTTOM = vmath.vector3(0, -collisions.bottom, 0)
+		BOUNDS_TOP = vmath.vector3(0, collisions.top, 0)
+		BOUNDS_LEFT = vmath.vector3(-collisions.left, 0, 0)
+		BOUNDS_RIGHT = vmath.vector3(collisions.right, 0, 0)
+	
+		local RAY_CAST_LEFT = BOUNDS_LEFT + vmath.vector3(-1, 0, 0)
+		local RAY_CAST_RIGHT = BOUNDS_RIGHT + vmath.vector3(1, 0, 0)
+		local RAY_CAST_DOWN = BOUNDS_BOTTOM + vmath.vector3(0, -1, 0)
+		local RAY_CAST_UP = BOUNDS_TOP + vmath.vector3(0, 1, 0)
+		local RAY_CAST_DOWN_LEFT = RAY_CAST_LEFT + RAY_CAST_DOWN
+		local RAY_CAST_DOWN_RIGHT = RAY_CAST_RIGHT + RAY_CAST_DOWN
+		local RAY_CAST_UP_LEFT = RAY_CAST_UP + RAY_CAST_LEFT
+		local RAY_CAST_UP_RIGHT = RAY_CAST_UP + RAY_CAST_RIGHT
 
-	-- order of ray casts is important!
-	-- we need to check for wall contact before checking ground
-	-- contact to be able to handle collision separation properly
-	local RAYS = {
-		{ id = RAY_CAST_LEFT_ID, ray = RAY_CAST_LEFT },
-		{ id = RAY_CAST_RIGHT_ID, ray = RAY_CAST_RIGHT },
-		{ id = RAY_CAST_UP_ID, ray = RAY_CAST_UP },
-		{ id = RAY_CAST_UP_LEFT_ID, ray = RAY_CAST_UP_LEFT },
-		{ id = RAY_CAST_UP_RIGHT_ID, ray = RAY_CAST_UP_RIGHT },
-		{ id = RAY_CAST_DOWN_ID, ray = RAY_CAST_DOWN },
-		{ id = RAY_CAST_DOWN_LEFT_ID, ray = RAY_CAST_DOWN_LEFT },
-		{ id = RAY_CAST_DOWN_RIGHT_ID, ray = RAY_CAST_DOWN_RIGHT },
-	}
+		-- order of ray casts is important!
+		-- we need to check for wall contact before checking ground
+		-- contact to be able to handle collision separation properly
+		RAYS = {
+			RAY_CAST_LEFT, RAY_CAST_RIGHT, RAY_CAST_UP, RAY_CAST_UP_LEFT,
+			RAY_CAST_UP_RIGHT, RAY_CAST_DOWN, RAY_CAST_DOWN_LEFT, RAY_CAST_DOWN_RIGHT
+		}
+	end
+
+	platypus.set_collisions(platypus.collisions)
 
 	local function check_group_direction(group, direction)
 		return bit.band(config.collisions.groups[group], direction) > 0
@@ -190,7 +194,7 @@ function M.create(config)
 			-- up-hill
 			elseif state.slope_left then
 				movement.y = -velocity * math.abs(state.slope_left.y)
-				movement.x = -velocity * math.abs(state.slope_left.x)
+				movement.x = -velocity * (1 - math.abs(state.slope_left.x))
 			end
 		elseif state.wall_contact ~= -1 and platypus.allow_wall_slide and platypus.is_falling() and not state.wall_slide then
 			state.wall_slide = true
@@ -211,7 +215,7 @@ function M.create(config)
 			-- up-hill
 			if state.slope_right then
 				movement.y = -velocity * math.abs(state.slope_right.y)
-				movement.x = velocity * math.abs(state.slope_right.x)
+				movement.x = velocity * (1 - math.abs(state.slope_right.x))
 			-- down-hill
 			elseif state.slope_left then
 				movement.y = -velocity * math.abs(state.slope_left.y)
@@ -348,9 +352,9 @@ function M.create(config)
 		local previous_wall_contact = state.wall_contact
 		state.wall_contact = nil
 		state.ground_contact = false
-		for _,r in ipairs(RAYS) do
-			local ray = r.ray
-			local id = r.id
+		for id, ray in ipairs(RAYS) do
+			--local ray = r.ray
+			--local id = r.id
 			local result = raycast(id, raycast_origin + offset, raycast_origin + offset + ray)
 			if result then
 				local separation = ray * (1 - result.fraction)
